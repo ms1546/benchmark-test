c273e8a665defc1de2006181111597f9a59c3292
ms <maemotosato@gmail.coms>
2024-11-19 23:42:39 +0900

    up

--- benchmark.js
+++ benchmark.js
@@ -1,21 +1,29 @@
-const { Suite } = require('benchmark');
-const testFunction = require('./index');
+const fs = require('fs');
+const Benchmark = require('benchmark');
+const fn = require('./index');
 
 const longString = 'test'.repeat(1000000);
 
 const userName = process.env.USER_NAME || 'anonymous';
 
-const suite = new Suite();
+const suite = new Benchmark.Suite();
 suite
-  .add(`${userName}の処理`, () => {
-    testFunction(longString);
-  })
-  .on('cycle', event => {
-    console.log(String(event.target));
+  .add(userName, () => {
+    fn(longString);
   })
   .on('complete', function () {
-    const result = this[0];
-    const executionTime = (1 / result.hz) * 1000;
-    console.log(`${userName}の実行速度: ${executionTime.toFixed(2)}ミリ秒`);
+    const executionTime = (1 / this[0].hz) * 1000;
+
+    let results = [];
+    try {
+      results = JSON.parse(fs.readFileSync('results.json', 'utf8'));
+    } catch (error) {
+      console.log('No existing results, creating new one.');
+    }
+
+    results.push({ user: userName, time: executionTime });
+    fs.writeFileSync('results.json', JSON.stringify(results, null, 2));
+
+    console.log(`${userName}の実行速度: ${executionTime.toFixed(2)} ms`);
   })
   .run({ async: true });
