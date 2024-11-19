75d037d82513204a0469a5323aee9cdd10f1c65c
ms <maemotosato@gmail.coms>
2024-11-20 00:04:48 +0900

    test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function (str) {
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 1; i++) {
         console.log(str);
     }
     return str.split('').reverse().join('');
